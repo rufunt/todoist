5ad0656249feeddd479a6c1c5dee0412829b602f
rufunt <jazzoo@mail.ru>
2018-01-14 01:31:29 +0300

    add View files

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,11 @@
 class ItemsController < ApplicationController
+  before_action :find_item, only: [:show, :edit, :update, :destroy]
 
   def index
     @items = Item.all.order("created_at DESC")
+  end
+
+  def show
   end
 
   def new
@@ -21,6 +25,10 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:title,:description)
+  end
+
+  def find_item
+    @item = Item.find(params[:id])
   end
 
 end
